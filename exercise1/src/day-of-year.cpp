e8b15da3c9d501e0c228a32334a51a1721d61786
MBronko <michal.brono@protonmail.com>
2023-06-06 18:45:16 +0200

    More readable constants

--- exercise1/src/day-of-year.cpp
+++ exercise1/src/day-of-year.cpp
@@ -3,18 +3,19 @@
 #include <array>
 
 
-constexpr int FOURHUNDRED = 400;
-constexpr int ONEHUNDRED  = 100;
+constexpr int leapYearException    = 400;
+constexpr int nonLeapYearException = 100;
 
 int dayOfYear(int month, int dayOfMonth, int year) {
-    const std::array<int, 11> monthDays = {31,28,31,30,31,30,31,31,30,31,30};
+    const std::array<int, 11> monthDays = {31, 28, 31, 30, 31, 30,
+                                           31, 31, 30, 31, 30};
     for(int i = 0; i < month - 1; i++) {
         dayOfMonth += monthDays.at(i);
     }
 
-    if(month > 2 && year % 4 == 0 && (year % ONEHUNDRED != 0 || year % FOURHUNDRED == 0)) {
+    if(month > 2 && year % 4 == 0 &&
+       (year % nonLeapYearException != 0 || year % leapYearException == 0)) {
         dayOfMonth++;
     }
     return dayOfMonth;
 }
-
