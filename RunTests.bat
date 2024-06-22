6948818bdfd6c3dba454e640cb5575598d719a10
cxnajder <259448@student.pwr.edu.pl>
2024-06-22 15:20:19 +0200

    Add bat scripts

---
+++ RunTests.bat
@@ -0,0 +1,20 @@
+@echo ==================
+@echo Excercise 1 Tests:
+@echo ==================
+@build\exercise1\DayOfYearTest.exe --gtest_color=yes
+
+@echo ==================
+@echo Excercise 2 Tests:
+@echo ==================
+@echo NA
+
+@echo ==================
+@echo Excercise 3 Tests:
+@echo ==================
+@echo NA
+
+@echo ==================
+@echo Excercise 4 Tests:
+@echo ==================
+@echo NA
+
