134db77426bb9b08d2c320abcfda0ef0f4f01c26
cxnajder <259448@student.pwr.edu.pl>
2024-07-01 01:35:14 +0200

    Prettify script prints

--- RunTests.sh
+++ RunTests.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-echo ==================
-echo Excercise 1 Tests:
-echo ==================
+echo ===================
+echo  Excercise 1 Tests 
+echo ===================
 ./build/exercise1/DayOfYearTests --gtest_color=yes
 
-echo ==================
-echo Excercise 2 Tests:
-echo ==================
+echo ===================
+echo  Excercise 2 Tests 
+echo ===================
 echo NA
 
-echo ==================
-echo Excercise 3 Tests:
-echo ==================
+echo ===================
+echo  Excercise 3 Tests 
+echo ===================
 echo NA
 
-echo ==================
-echo Excercise 4 Tests:
-echo ==================
+echo ===================
+echo  Excercise 4 Tests 
+echo ===================
 echo NA
 
