7114c37b719183e213d9174130eae60810e9c8f6
cxnajder <259448@student.pwr.edu.pl>
2024-07-02 22:59:06 +0200

    Prettify windows build script

--- WindowsBuild.bat
+++ WindowsBuild.bat
@@ -1,4 +1,8 @@
 @echo on
+@echo =================== Git: Downloading Submodules ====================
 git submodule update --init --recursive
+@echo =================== CMake: Preparing Build Files ===================
 cmake -S . -B build
+@echo =================== Ninja: Building ================================
 ninja -C build
+@echo ====================================================================
