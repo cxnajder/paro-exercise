c626fc39d101cea59aa0c7234b11dd48257eca13
cxnajder <259448@student.pwr.edu.pl>
2024-07-02 20:44:24 +0200

    Make scripts automatically download submodules

--- LinuxBuild.sh
+++ LinuxBuild.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+git submodule update --init --recursive
 cmake -S . -B build
 make -C build
--- WindowsBuild.bat
+++ WindowsBuild.bat
@@ -1,3 +1,4 @@
 @echo on
+git submodule update --init --recursive
 cmake -S . -B build
 ninja -C build
