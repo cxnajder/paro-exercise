ba8f17134e1a87a36dd87cd59ade122304a7c00e
cxnajder <259448@student.pwr.edu.pl>
2024-07-02 23:01:36 +0200

    Prettify linux build script

--- LinuxBuild.sh
+++ LinuxBuild.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo =================== Git: Downloading Submodules ====================
 git submodule update --init --recursive
+echo =================== CMake: Preparing Build Files ===================
 cmake -S . -B build
+echo =================== Make: Building =================================
 make -C build
+echo ====================================================================
