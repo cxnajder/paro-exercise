83c0b2c2a9a834f50e04badb4238daecdf807da1
cxnajder <259448@student.pwr.edu.pl>
2024-07-02 21:36:29 +0200

    Add letters and points map

--- exercise2/src/scrabbleScore.cpp
+++ exercise2/src/scrabbleScore.cpp
@@ -1,6 +1,27 @@
 #include "scrabbleScore.h"
+#include<map>
+#include <vector>
 
-int cxnajdersCode::countScrabbleScore(const std::string& word)
-{
-    return word.size(); // FOR NOW
+namespace cxnajdersCode {
+    // I like to make my code very explicit and easy to read.
+    // I am aware it can be also very tedious and less memory/speed efficient.
+
+    namespace {
+        // empty namespace makes it available only in this unit (just like static keytword in c)
+
+        const std::map<std::vector<char>, int> cLettersAndPointsMap = { 
+            { { 'A', 'E', 'I', 'O', 'U', 'L','N', 'R', 'S', 'T' }, 1 },
+            { { 'D', 'G' }, 2 },
+            { { 'B', 'C', 'M', 'P' }, 3 },
+            { { 'F', 'H', 'V', 'W', 'Y' }, 4 },
+            { { 'K' }, 5 },
+            { { 'J', 'X' }, 8 },
+            { { 'Q', 'Z' }, 10 },
+        };
+    }
+
+    int countScrabbleScore(const std::string& word)
+    {
+        return word.size(); // FOR NOW
+    }
 }
