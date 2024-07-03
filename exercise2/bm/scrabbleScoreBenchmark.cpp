8f9caa719c9417d9effa8e14aed97c4419a5c410
cxnajder <259448@student.pwr.edu.pl>
2024-07-03 20:44:06 +0200

    Add optimized examples

--- exercise2/bm/scrabbleScoreBenchmark.cpp
+++ exercise2/bm/scrabbleScoreBenchmark.cpp
@@ -8,6 +8,13 @@ static void cxnajderScrabbleBench_cabbage(benchmark::State &s)
         benchmark::DoNotOptimize(cxnajdersCode::countScrabbleScore("cabbage"));
     }
 }
+static void cxnajderScrabbleBench_cabbage_optimal(benchmark::State &s)
+{
+    for (auto _ : s)
+    {
+        cxnajdersCode::countScrabbleScore("cabbage");
+    }
+}
 
 static void cxnajderScrabbleBench_CABBAGE(benchmark::State &s)
 {
@@ -16,6 +23,15 @@ static void cxnajderScrabbleBench_CABBAGE(benchmark::State &s)
         benchmark::DoNotOptimize(cxnajdersCode::countScrabbleScore("CABBAGE"));
     }
 }
+static void cxnajderScrabbleBench_CABBAGE_optimal(benchmark::State &s)
+{
+    for (auto _ : s)
+    {
+        cxnajdersCode::countScrabbleScore("CABBAGE");
+    }
+}
 BENCHMARK(cxnajderScrabbleBench_cabbage);
+BENCHMARK(cxnajderScrabbleBench_cabbage_optimal);
 BENCHMARK(cxnajderScrabbleBench_CABBAGE);
+BENCHMARK(cxnajderScrabbleBench_CABBAGE_optimal);
 BENCHMARK_MAIN();
